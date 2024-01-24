1551322dc44c05b7d8b1b173f0d1e0fb3922c3a8
DavidPC5 <157392601+DavidPC5@users.noreply.github.com>
2024-01-24 16:05:36 +0000

    Did M1Lab

---
+++ M1LAB_Plata.cpp
@@ -0,0 +1,28 @@
+//CSC 134
+//M1LAB - Apple Sales
+//David Plta Cruz
+//1/24/24
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "M1Lab - Apple Sales" << endl;
+    //Simulate selling apples
+    //Set up variables (nouns)
+    string name = "David";
+    int num_apples = 20;
+    double price_per_apple = 0.25;
+
+    //Do the calculations
+    double total_price = 0;
+    //NOTE: math operations are + - * /
+    total_price = num_apples * price_per_apple;
+
+    //Print the output
+    cout << "Welcome to " << name << "'s Apple Orchard" << endl;
+    cout << "We have " << num_apples << " apples for sale." << endl;
+    cout << "At a cost of $" << price_per_apple << " each." << endl;
+    cout << "For a total cost of $" << total_price << endl;
+
+    return 0;
+}
