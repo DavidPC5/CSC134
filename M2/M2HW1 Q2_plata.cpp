074ad5ef94296f93bb48a954b83265fcd07b9472
DavidPC5 <157392601+DavidPC5@users.noreply.github.com>
2024-02-18 13:50:14 +0000

    done

--- M2/M2HW1 Q2_plata.cpp
+++ M2/M2HW1 Q2_plata.cpp
@@ -11,12 +11,36 @@ using namespace std;
 
 int main()
 {
+    cout << "General Crates Pricing System" << endl;
+    //Declare variables
+    double length, width, height; //in feet
+    double volume; //in cubic feet
+    double cost, charge, profit; // in $
 
+    //Declare constants for cost and amount charged
+    const double COST_PER_CUBIC_FOOT = 0.3;
+    const double CHARGE_PER_CUBIC_FOOT = 0.51;
+   
+    //Ask for user input
+    cout << "Length: ";
+    cin >> length;
+    cout << "Width: ";
+    cin >> width;
+    cout << "Height: ";
+    cin >> height;
 
+    //Do calculations  
+   volume = length * width * height;
+   cost = volume * COST_PER_CUBIC_FOOT;
+   charge = volume * CHARGE_PER_CUBIC_FOOT;
+   profit = charge - cost;
 
-
-
-
-
-return 0;
+    //print the answer
+    cout << "The crates volume is: " << volume << " cubic feet" << endl;
+    cout << fixed << setprecision(2);
+    cout << "Cost to build it is: $" << cost << endl;
+    cout << "Charge to customer is: $" << charge << endl;
+    cout << "Our Profit: $" << profit << endl;
+    
+    return 0;
 }
