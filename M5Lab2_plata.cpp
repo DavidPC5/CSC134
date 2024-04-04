ed6b381bcc85bfa2891aa3284fc454209ecbed73
DavidPC5 <157392601+DavidPC5@users.noreply.github.com>
2024-04-04 22:36:33 +0000

    d

--- M5Lab2_plata.cpp
+++ M5Lab2_plata.cpp
@@ -1,39 +1,41 @@
-// M5LAB2
-// Header goes here
+/*
+CSC 134
+M5Lab2
+David Plata Cruz
+4/4/24
+*/
 
 #include <iostream>
 using namespace std;
 
-// Declare (Write the prototypes for)
-// the getLength,
-// getWidth, getArea, and displayData
-// functions here.
+//functions prototypes
+void getLength();
+void getWidth();
+void getArea();
+void displayData();
 
 int main()
-{
-	// This program calculates the area of a rectangle.
-	// TODO: fix any syntax errors
-	
-   double length,    // The rectangle's length
-          width,     // The rectangle's width
-          area;      // The rectangle's area
+{	
+    //delcare variables 
+    double length, width, area; 
           
-   // Get the rectangle's length.
-   length = getLength();
+    length = getLength();
+    width = getWidth()
+    area = getArea();
    
-   // Get the rectangle's width.
-   width = getWidth()
-   
-   // Get the rectangle's area.
-   are = getArea();
-   
-   // Display the rectangle's data.
-   displayData(length, width, area);
+    // Display the rectangle's data.
+    displayData(length, width, area);
           
-   return 0;
+    return 0;
 }
 
-//***************************************************
-// TODO: write the getLength, getWidth, getArea,    *
-// and displayData functions below.                 *
-//*****************************
+/*
+void getLength()
+
+void getWidth()
+
+void getArea()
+
+void displayData()
+
+*/
